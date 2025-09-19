dd4a93f0964f2f36e77dc726df5f86304f9e8b20
Anurag Basuri <anuragbasuri2502@gmail.com>
2025-09-19 09:24:24 +0530

    hkfh

--- server/src/routes/questionRoutes.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import { addQuestion } from '../controllers/questionController.js';
-import auth from '../middlewares/authmiddleware.js';
-
-const router = express.Router();
-
-router.post('/:examId/questions', auth, addQuestion);
-
-export default router;
